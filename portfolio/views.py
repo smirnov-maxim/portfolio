4d6e4993f26525d16626366121e71ea7ee0a4118
Maxim Smirnov <smirnov_maksim08@mail.ru>
2022-01-09 14:20:41 +0300

    second commit

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, 'portfolio/main_page.html')
+
